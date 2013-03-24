33f49db02242b110ccd8baf2c92e4ab67f77d983
Charlie McClung <charlie@cmr1.com>
2013-03-24 01:56:19 -0600

    Updated style.less

--- globals/header.php
+++ globals/header.php
@@ -38,9 +38,9 @@ defined( '_CN_EXEC' ) or die( 'Restricted Access' );
 			<?php // Begin HTML header tag ?>	
 			<header id="header">
 				<div id="userdata">
-					<a href="<?php echo WEBROOTPAGE; ?>signup.php" class="button">Sign Up!</button>
+					<a href="<?php echo WEBROOTPAGE; ?>signup.php" class="button">Sign Up!</a>
 					OR 
-					<a href="<?php echo WEBROOTPAGE; ?>login.php" class="button">Login</button>
+					<a href="<?php echo WEBROOTPAGE; ?>login.php" class="button">Login</a>
 				</div>
 				<hgroup>
 					<h1>CNerdForum</h1>
