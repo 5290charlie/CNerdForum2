f99e1ca53d401ee46d4e64dc1b41d6ed0860aa92
Charlie McClung <charlie@cmr1.com>
2013-03-26 04:07:16 -0600

    Building Forum system: debugging topic.php

--- topic.php
+++ topic.php
@@ -6,21 +6,7 @@ $cn =& CN::getInstance();
 $cn->init();
 
 if( !empty( $_POST ) && !empty( $_POST['topic_id'] ) && !empty( $_POST['user_id'] ) && !empty( $_POST['title'] ) && !empty( $_POST['details'] ) ) {
-	if ( CN_Post::add( $_POST ) ) {
-		$cn->enqueueMessage(
-			'Successfully created post: ' . $_POST['title'],
-			CN_MSG_SUCCESS,
-			$_SESSION['sessionID']
-		);
-	} else {
-		$cn->enqueueMessage(
-			'Error creating post: ' . $_POST['title'],
-			CN_MSG_ERROR,
-			$_SESSION['sessionID']
-		);
-	}
-	
-	CN::redirect( CN_WEBROOTPAGE . 'topic?tid=' . $_POST['topic_id'] );
+
 } 
 
 if( !empty( $_GET ) && !empty( $_GET['tid'] ) ) {
