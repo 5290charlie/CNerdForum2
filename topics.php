c19361124bcc08d113469f371ef96154a341d83e
Charlie McClung <charlie@cmr1.com>
2013-03-26 13:25:07 -0600

    Change topics display to table

--- topics.php
+++ topics.php
@@ -30,18 +30,36 @@ require_once( CN_DIR_GLOBALS . 'header.php' );
 ?>
 				<h1>Topics</h1>
 				<hr>
-				<ul>
+				<table id="topics">
 				<?php foreach( $topics as $t ) { ?>
-					<li>
-						<strong>Title:</strong> <a href="<?php echo CN_WEBROOTPAGE . 'topic?tid=' . $t->id; ?>"><?php echo $t->title; ?></a> 
-						(<a href="<?php echo CN_WEBROOTPAGE . 'touch?tid=' . $t->id; ?>">touch</a>)<br />
-						<strong>Details:</strong> <?php echo $t->details; ?><br />
-						<strong>Date:</strong> <?php echo date( CN_DATE_FORMAT, $t->date ); ?><br />
-						<strong>Updated:</strong> <?php echo date( CN_DATE_FORMAT, $t->updated ); ?><br />
-						<strong>Author:</strong> <?php echo $t->author->username . ' (' . $t->author->firstname . ' ' . $t->author->lastname . ')'; ?>
-					</li>
+					<tr class="topic">
+						<td class="title">
+							<a href="<?php echo CN_WEBROOTPAGE . 'topic?tid=' . $t->id; ?>">
+								<strong><?php echo $t->title; ?></strong>
+							</a>
+							<div class="smallfont">
+								<?php echo $t->details; ?>
+							</div>
+						</td>
+						<td class="stats">
+							<div class="smallfont">
+								Posts: <strong><?php echo count( $t->getPosts() ); ?></strong>
+							</div>
+						</td>
+						<td class="details">
+							<div class="smallfont">
+								Created: <?php echo date( CN_DATE_FORMAT, $t->date ); ?>
+							</div>
+							<div class="smallfont">
+								Updated: <?php echo date( CN_DATE_FORMAT, $t->updated ); ?>
+							</div>
+							<div class="smallfont">
+								Author: <?php echo $t->author->username . ' (' . $t->author->firstname . ' ' . $t->author->lastname . ')'; ?>
+							</div>
+						</td>
+					</tr>
 				<?php } ?>
-				</ul>
+				</table>
 				<hr>
 				<form id="new_topic" method="post" action="<?php echo CN_WEBROOTPAGE . 'topics'; ?>">
 					<input type="hidden" id="user_id" name="user_id" value="<?php echo $user->id; ?>" />
