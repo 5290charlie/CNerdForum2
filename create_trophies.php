f2f39df1b6d49fc0202182486fb53e5d698056c0
Charlie McClung <charlie@cmr1.com>
2013-03-29 13:10:43 -0600

    update to trophy creation

--- create_trophies.php
+++ create_trophies.php
@@ -7,97 +7,97 @@ $trophies = array(
 	array(
 		'mana' => 0,
 		'rank' => 'n00b',
-		'icon' => 'n00b.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'n00b.png'
 	),
 	// punk rank
 	array(
 		'mana' => 50,
 		'rank' => 'punk',
-		'icon' => 'punk.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'punk.png'
 	),
 	// greenhorn rank
 	array(
 		'mana' => 100,
 		'rank' => 'greenhorn',
-		'icon' => 'greenhorn.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'greenhorn.png'
 	),
 	// apprentice rank
 	array(
 		'mana' => 200,
 		'rank' => 'apprentice',
-		'icon' => 'apprentice.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'apprentice.png'
 	),
 	// pro rank
 	array(
 		'mana' => 300,
 		'rank' => 'pro',
-		'icon' => 'pro.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'pro.png'
 	),
 	// engineer rank
 	array(
 		'mana' => 400,
 		'rank' => 'engineer',
-		'icon' => 'engineer.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'engineer.png'
 	),
 	// master rank
 	array(
 		'mana' => 500,
 		'rank' => 'master',
-		'icon' => 'master.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'master.png'
 	),
 	// elder rank
 	array(
 		'mana' => 600,
 		'rank' => 'elder',
-		'icon' => 'elder.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'elder.png'
 	),
 	// wiseman rank
 	array(
 		'mana' => 700,
 		'rank' => 'wiseman',
-		'icon' => 'wiseman.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'wiseman.png'
 	),
 	// prince rank
 	array(
 		'mana' => 800,
 		'rank' => 'prince',
-		'icon' => 'prince.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'prince.png'
 	),
 	// king rank
 	array(
 		'mana' => 900,
 		'rank' => 'king',
-		'icon' => 'king.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'king.png'
 	),
 	// lord rank
 	array(
 		'mana' => 1000,
 		'rank' => 'lord',
-		'icon' => 'lord.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'lord.png'
 	),
 	// geek rank
 	array(
 		'mana' => 1500,
 		'rank' => 'geek',
-		'icon' => 'geek.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'geek.png'
 	),
 	// computer nerd rank
 	array(
 		'mana' => 2000,
 		'rank' => 'computer nerd',
-		'icon' => 'computer nerd.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'computer nerd.png'
 	),
 	// genius rank
 	array(
 		'mana' => 2500,
 		'rank' => 'genius',
-		'icon' => 'genius.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'genius.png'
 	),
 	// evil genius rank
 	array(
 		'mana' => 5000,
 		'rank' => 'evil genius',
-		'icon' => 'evil genius.png'
+		'icon' => CN_WEBDIR_TROPHIES . 'evil genius.png'
 	)
 );
 
@@ -107,8 +107,8 @@ foreach( $trophies as $trophy ) {
 	else
 		echo 'Failed to add trophy!!!';
 	
-	echo '\tMana: ' . $trophy['mana'] . '<br />';
-	echo '\tRank: ' . $trophy['rank'] . '<br />';
-	echo '\tIcon: ' . $trophy['icon'] . '<br />';
+	echo "\tMana: " . $trophy['mana'] . '<br />';
+	echo "\tRank: " . $trophy['rank'] . '<br />';
+	echo "\tIcon: " . $trophy['icon'] . '<br />';
 }
 ?>
