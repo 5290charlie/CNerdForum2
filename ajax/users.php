52a1139d8a10661bbc92c3ae064e19d881619dfc
Charlie McClung <charlie@cmr1.com>
2013-04-24 23:03:42 -0600

    updated users page

--- ajax/users.php
+++ ajax/users.php
@@ -10,47 +10,43 @@
 
 // Initialize users object with all users
 $disabled = CN_User::getPermission( CN_PERM_DISABLED );
-$users = CN_User::getPermission( CN_PERM_USER );
+$regular = CN_User::getPermission( CN_PERM_USER );
 $mods = CN_User::getPermission( CN_PERM_MOD );
 $admins = CN_User::getPermission( CN_PERM_ADMIN );
+
+$usergroups = array(
+	'disabled' => array(
+		'title' => 'Disabled Users',
+		'empty' => 'No Disabled Users',
+		'users' => $disabled
+	),
+	'regular' => array(
+		'title' => 'Regular Users',
+		'empty' => 'No Regular Users',
+		'users' => $regular
+	),
+	'mod' => array(
+		'title' => 'Moderators',
+		'empty' => 'No Moderators',
+		'users' => $mods
+	),
+	'admin' => array(
+		'title' => 'Administrators',
+		'empty' => 'No Administrators',
+		'users' => $admins
+	),
+);
+
+foreach ( $usergroups as $group => $info ) {
 ?>
-	<h2>Disabled Users</h2>
-	<div id="disabled-users">
-	<?php if ( count( $disabled ) > 0 ) {
-		foreach( $disabled as $user ) { ?>
-			<div class="user" onclick="window.location='<?php echo CN_WEBACCOUNT . '?user=' . $user->username; ?>'">
-				<?php echo $user->fullname; ?>
+	<h2><?php echo $info['title']; ?></h2>
+	<div id="<?php echo $group; ?>-users">
+	<?php if ( count( $info['users'] ) > 0 ) {
+		foreach( $info['users'] as $user ) { ?>
+			<div class="user">
+				<?php echo $user->fullname; ?> | <a href="<?php echo CN_WEBACCOUNT . '?user=' . $user->username; ?>"><?php echo $user->username; ?></a>
 			</div>
 		<?php } 
-	} else { echo '<p>No Disabled Users</p>'; } ?>
+	} else { echo '<p>' . $info['empty'] . '</p>'; } ?>
 	</div>
-	<h2>Regular Users</h2>
-	<div id="regular-users">
-	<?php if ( count( $users ) > 0 ) {
-		foreach( $users as $user ) { ?>
-			<div class="user" onclick="window.location='<?php echo CN_WEBACCOUNT . '?user=' . $user->username; ?>'">
-				<?php echo $user->fullname; ?>
-			</div>
-		<?php } 
-	} else { echo '<p>No Regular Users</p>'; } ?>
-	</div>
-	<h2>Moderators</h2>
-	<div id="mod-users">
-	<?php if ( count( $mods ) > 0 ) {
-		foreach( $$mods as $user ) { ?>
-			<div class="user" onclick="window.location='<?php echo CN_WEBACCOUNT . '?user=' . $user->username; ?>'">
-				<?php echo $user->fullname; ?>
-			</div>
-		<?php } 
-	} else { echo '<p>No Moderators</p>'; } ?>
-	</div>
-	<h2>Administrators</h2>
-	<div id="admin-users">
-	<?php if ( count( $admins ) > 0 ) {
-		foreach( $admins as $user ) { ?>
-			<div class="user" onclick="window.location='<?php echo CN_WEBACCOUNT . '?user=' . $user->username; ?>'">
-				<?php echo $user->fullname; ?>
-			</div>
-		<?php } 
-	} else { echo '<p>No Administrators</p>'; } ?>
-	</div>
+	
