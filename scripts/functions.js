2969eaa2a47e0a6be11fe1de90ecb251b2211926
Charlie McClung <charlie@cmr1.com>
2013-04-24 21:08:28 -0600

    added tabs

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,8 +1,27 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
+    $("#content div").hide(); // Initially hide all content
+    $("#tabs li:first").attr("id","current"); // Activate first tab
+    $("#content div:first").fadeIn(); // Show first tab content
+    
+    $('#tabs a').click(function(e) {
+        e.preventDefault();
+        if ($(this).closest("li").attr("id") == "current"){ //detection for current tab
+         return       
+        }
+        else{             
+        $("#content div").hide(); //Hide all content
+        $("#tabs li").attr("id",""); //Reset id's
+        $(this).parent().attr("id","current"); // Activate this
+        $('#' + $(this).attr('name')).fadeIn(); // Show content for current tab
+        }
+    });
+});
+
+//jQuery(document).ready(function() {
 //	$("#tabs").tabs();
 //	$(".accordion").accordion();
 //	$("button, .button").button();
-});
+//});
 
 function voteComment( cid, val ) {
 /*	$url = '/ajax/vote';
